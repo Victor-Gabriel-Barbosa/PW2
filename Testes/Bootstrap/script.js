6f180836b5ca3b73a82e95138ea2ea663ee4664f
Victor <victorgabrielbarbosa88@gmail.com>
2025-06-21 21:52:36 -0300

    feat: Atualiza o layout da navbar e adiciona novas seções com funcionalidades de Bootstrap

---
+++ Testes/Bootstrap/script.js
@@ -0,0 +1,18 @@
+// Inicializa tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+// Inicializa popovers
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+// Configura toast
+const toastTrigger = document.getElementById('liveToastBtn');
+const toastLiveExample = document.getElementById('liveToast');
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show();
+  });
+}
